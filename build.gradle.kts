5899a867afa0f75235105bce8fddf71117f9e680
Mohammad Mostafa <ezmmd83@gmail.com>
2022-05-17 18:54:54 +0430

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+	kotlin("jvm") version "1.6.21"
+	`java-library`
 }
 
 group = "ir.mmd.ktdev"
 version = "0.2.0"
 
+java {
+	withSourcesJar()
+}
+
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+	testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-        freeCompilerArgs = listOf("-Xcontext-receivers")
-    }
+	kotlinOptions {
+		jvmTarget = JavaVersion.VERSION_17.toString()
+		freeCompilerArgs = listOf("-Xcontext-receivers")
+	}
 }
